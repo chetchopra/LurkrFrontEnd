fb9b088aa1796fb6241b1a43bad12727e0230eb3
chetchopra <chetanya.chopra94@gmail.com>
2019-07-31 13:21:09 -0700

    Basic username login work but still no routes

--- lurkr/src/App.js
+++ lurkr/src/App.js
@@ -16,66 +16,26 @@ import MainStage from './conatiners/MainStage'
 export default class App extends Component {
     constructor() {
     super()
-    let themeBlues = {
-      header: "#96858F",
-      backGround: "#6D7993",
-      subreddit: "#9099A2",
-      post: "#D5D5D5"
-    }
-    let themeRustic = {
-      header: "#18121E",
-      backGround: "#233237",
-      subreddit: "#984B43",
-      post: "#D5D5D5"    
-      // "#EAC67A"
-    }
-    let themeOrangeDelight = {
-      header: "#6B7A8F",
-      backGround: "#F7882F",
-      subreddit: "#F7C331",
-      post: "#DCC7AA"
-    }
-    let themeDarkMelon = {
-      header: "#CF6766",
-      backGround: "#031424",
-      subreddit: "#30415D",
-      post: "#8EAEBD"
-    }
-    let themeModernHome = {
-      header: "#DA635D",
-      backGround: "#4E4E56",
-      subreddit: "#DCD0C0",
-      post: "#B1938B"
-    }
-    let themeDark = {
-      header: "#000000",
-      backGround: "#4E4E56",
-      subreddit: "#76323F",
-      post: "#B1938B"
-    }
-    let themeUglyDuckling = {
-      header: "#D3D3D3",
-      backGround: "#FFFFFF",
-      subreddit: "#FFFFFF",
-      post: "#FFFFFF"
-    }
-
-
-
     this.state = {
-      subreddits: [],
-      searchFieldValue: "",
       usernameFieldValue: "",
-      theme: {...themeUglyDuckling},
-      currentUser: null
-      
+      currentUser: {
+        username: null,
+        theme: null,
+        id: null
+      } 
     }
   }
 
   componentDidMount() {
-    fetch("http://localhost:3000/users/1")
-    .then(resp => resp.json())
-    .then(json => this.setState({subreddits: json.subreddits}))
+    let username = localStorage.getItem("currentUsername")
+    let userId = localStorage.getItem("currentUserId")
+    console.log(username !== null && userId !== null)
+    if (username !== null && userId !== null) {
+      let newCurrentUser = {...this.state.currentUser}
+      newCurrentUser.username = username;
+      newCurrentUser.id = userId;
+      this.setState({currentUser: newCurrentUser})
+    }
   }
 
   handleLogin = (event) => {
@@ -85,25 +45,21 @@ export default class App extends Component {
     fetch(`http://localhost:3000/users/login/${username}`)
     .then(resp => resp.json())
     .then(json => {
-      console.log(json);
       this.handleLoginResponse(json);
       });
-    //fetch to see if user exists
-    //if yes then fetch users subreddits and other info and save user in local storage
   }
 
   handleLoginResponse = (data) => {
     if (data.message) {
       this.setState({usernameFieldValue: "Incorrect Username"})
     } else if (data.username) {
-      this.setState({currentUser: data.username}) 
-      localStorage.setItem("currentUser", this.state.currentUser); 
+      let newCurrentUser = {...this.state.currentUser}
+      newCurrentUser.username = data.username;
+      newCurrentUser.id = data.id;
+      this.setState({currentUser: newCurrentUser}) 
+      localStorage.setItem("currentUsername", this.state.currentUser.username); 
+      localStorage.setItem("currentUserId", this.state.currentUser.id)
     }
-  }
-
-  searchFieldChange = (event) => {
-    let newSearchTerm = event.target.value;
-    this.setState({searchFieldValue: newSearchTerm}) 
   }
 
   usernameFieldChange = (event) => {
@@ -111,62 +67,17 @@ export default class App extends Component {
     this.setState({usernameFieldValue: newUsername}) 
   }
 
-  findSubreddit = (event) => {
-    event.preventDefault();
-    let configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({"subreddit": {"name": this.state.searchFieldValue, "user_id": 1}})
-    }
-
-    fetch("http://localhost:3000/subreddits", configObj)
-    .then(resp => resp.json())
-    .then(json => {
-      if (json.name !== "Issue") {
-        let newState = [...this.state.subreddits]
-        newState.push(json);
-        this.setState({subreddits: newState})
-      } 
-    })
-    this.setState({searchFieldValue: ""})
-    
-  }
-
-  removeSubreddit = (subredditName) => {
-    console.log(subredditName)
-
-    let configObj = {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({"subreddit": {"name": subredditName, "user_id": 1}})
-    }
-
-    fetch("http://localhost:3000/subreddits", configObj)
-    .then(resp => resp.json())
-    .then(json => {
-      console.log("made it into json")
-      let newState = [...this.state.subreddits]
-      let updatedState = newState.filter((subreddit) => subreddit.name !== subredditName)
-      this.setState({subreddits: updatedState})
-    })
-  }
-
   checkLocalStorage = () => {
     return localStorage.getItem("currentUser") !== null;
   }
 
   checkLoggedIn = () => {
-    if (this.state.currentUser !== null || this.checkLocalStorage()) {
+    console.log(this.state.currentUser.username !== null)
+    if (this.state.currentUser.username !== null) {
       return (
         <Fragment>
-          <Header searchFieldValue={this.state.searchFieldValue} searchFieldChange={this.searchFieldChange} findSubreddit={this.findSubreddit} theme={this.state.theme}/>
-          <MainStage subreddits={this.state.subreddits} removeSubreddit={this.removeSubreddit} theme={this.state.theme}/>
+          {/* <Header searchFieldValue={this.state.searchFieldValue} searchFieldChange={this.searchFieldChange} findSubreddit={this.findSubreddit} theme={this.state.theme}/> */}
+          <MainStage currentUser={this.state.currentUser}/>
         </Fragment>
       );
     } else {
--- lurkr/src/conatiners/MainStage.jsx
+++ lurkr/src/conatiners/MainStage.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Subreddit from '../components/Subreddit'
 import BasicGrid from './BasicGrid'
 import { Container } from '@material-ui/core';
 import DGrid from '../components/dragndrop/DGrid'
 import ExpansionPanel from '../components/ExpansionPanel'
+import Header from '../components/Header';
 
 
 
@@ -11,11 +12,127 @@ import ExpansionPanel from '../components/ExpansionPanel'
 
 
 export default class MainStage extends Component {
+  constructor() {
+    super()
+
+    // Visual Fuckery
+    let themeBlues = {
+      header: "#96858F",
+      backGround: "#6D7993",
+      subreddit: "#9099A2",
+      post: "#D5D5D5"
+    }
+    let themeRustic = {
+      header: "#18121E",
+      backGround: "#233237",
+      subreddit: "#984B43",
+      post: "#D5D5D5"    
+      // "#EAC67A"
+    }
+    let themeOrangeDelight = {
+      header: "#6B7A8F",
+      backGround: "#F7882F",
+      subreddit: "#F7C331",
+      post: "#DCC7AA"
+    }
+    let themeDarkMelon = {
+      header: "#CF6766",
+      backGround: "#031424",
+      subreddit: "#30415D",
+      post: "#8EAEBD"
+    }
+    let themeModernHome = {
+      header: "#DA635D",
+      backGround: "#4E4E56",
+      subreddit: "#DCD0C0",
+      post: "#B1938B"
+    }
+    let themeDark = {
+      header: "#000000",
+      backGround: "#4E4E56",
+      subreddit: "#76323F",
+      post: "#B1938B"
+    }
+    let themeUglyDuckling = {
+      header: "#D3D3D3",
+      backGround: "#FFFFFF",
+      subreddit: "#FFFFFF",
+      post: "#FFFFFF"
+    }
+
+    // STATE
+    this.state = {
+      subreddits: [],
+      searchFieldValue: "",
+      theme: {...themeUglyDuckling},
+    }
+  }
+
+
+  componentDidMount() {
+    console.log(this.props)
+    fetch(`http://localhost:3000/users/${this.props.currentUser.id}`)
+    .then(resp => resp.json())
+    .then(json => this.setState({subreddits: json.subreddits}))
+  }
+
+  searchFieldChange = (event) => {
+    let newSearchTerm = event.target.value;
+    this.setState({searchFieldValue: newSearchTerm}) 
+  }
+
+  findSubreddit = (event) => {
+    event.preventDefault();
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({"subreddit": {"name": this.state.searchFieldValue, "user_id": this.props.currentUser.id}})
+    }
+
+    fetch("http://localhost:3000/subreddits", configObj)
+    .then(resp => resp.json())
+    .then(json => {
+      if (json.name !== "Issue") {
+        let newState = [...this.state.subreddits]
+        newState.push(json);
+        this.setState({subreddits: newState})
+      } 
+    })
+    this.setState({searchFieldValue: ""})
+    
+  }
+
+  removeSubreddit = (subredditName) => {
+    console.log(subredditName)
+    let configObj = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({"subreddit": {"name": subredditName, "user_id": this.props.currentUser.id}})
+    }
+
+    fetch("http://localhost:3000/subreddits", configObj)
+    .then(resp => resp.json())
+    .then(json => {
+      console.log("made it into json")
+      let newState = [...this.state.subreddits]
+      let updatedState = newState.filter((subreddit) => subreddit.name !== subredditName)
+      this.setState({subreddits: updatedState})
+    })
+  }
 
 
   render() {
     return (
-      <BasicGrid subreddits={this.props.subreddits} removeSubreddit={this.props.removeSubreddit} theme={this.props.theme}/>  
+      <Fragment>
+        <Header searchFieldValue={this.state.searchFieldValue} searchFieldChange={this.searchFieldChange} findSubreddit={this.findSubreddit} theme={this.state.theme}/>
+        <BasicGrid subreddits={this.state.subreddits} removeSubreddit={this.removeSubreddit} theme={this.state.theme}/> 
+      </Fragment> 
     )
 
   }
